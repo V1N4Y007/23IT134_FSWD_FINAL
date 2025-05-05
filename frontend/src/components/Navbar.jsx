58f428d767032828d38429c69cf2e32b7641c6d4
Vinay <140490355+V1N4Y007@users.noreply.github.com>
2025-05-05 12:16:38 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          EMS-Abhishek Sharma
+          23IT134
         </Link>
         <button
           className="navbar-toggler"
